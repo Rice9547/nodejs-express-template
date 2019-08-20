e4a78b59707d39ff2e1d8d4d80b73550b40b18d0
黃曉敏 <rice@OuO.local>
2019-08-20 23:56:04 +0800

    removing node_modules

---
+++ controllers/user.js
@@ -0,0 +1,71 @@
+const jwt = require('jsonwebtoken');
+const models = require('../config/sequelize');
+const config = require('../config/settings');
+
+let list = async (req, res, next) => {
+    let data = await models.users.findAll({
+        attributes: {
+            exclude: ['password']
+        }
+    });
+    res.status(200).json({
+        status: true,
+        data: {
+            'message': data
+        }
+    });
+}
+
+let register = async (req, res, next) => {
+    try {
+        console.log(req.body, req.query)
+        await models.user.build({
+            email: req.body.email,
+            password: req.body.password
+        }).save();
+        res.status(200).json({
+            status: true,
+            data: {
+                'message': null
+            }
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: false,
+            data: {
+                'message': err
+            }
+        });
+    }
+}
+
+let login = async (req, res, next) => {
+    try {
+        let user = await models.users.find({
+            where: {
+                email: req.body.email,
+                password: req.body.password
+            }
+        });
+        let token = jwt.sign({ user: user.dataValues.id, email: user.dataValues.email }, config.salt, { expiresIn: 60 * 60 * 1000 });
+        res.status(200).json({
+            status: true,
+            data: {
+                'message': {
+                    token: token
+                }
+            }
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: false,
+            data: {
+                'message': err
+            }
+        });
+    }
+}
+
+module.exports = {
+    list, register, login
+};
---
+++ middlewares/authorization.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/settings');
+const salt = config.salt;
+
+module.exports = function (req, res, next) {
+    var token = req.headers['authorization'];
+    var flag = true;
+    if (!token || token == '') {
+        res.status(401).json({
+            status: false,
+            data: {
+                'message': 'token not provided'
+            }
+        });
+        return;
+    }
+    jwt.verify(token, salt, (err, decoded) => {
+        // other
+        if (err) {
+            res.status(401).json({
+                status: false,
+                data: {
+                    'message': 'token error'
+                }
+            });
+            return;
+        }
+        return next();
+    });
+};
